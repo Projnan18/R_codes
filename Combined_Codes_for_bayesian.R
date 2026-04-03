eb542626f911590ee46e49e3f9ff2d6036d8b8a5
Projnan18 <projnanduarah18@gmail.com>
2026-04-03 20:09:57 +0530

    Add files via upload

---
+++ Combined_Codes_for_bayesian.R
@@ -0,0 +1,27 @@
+# Load data
+data <- read.table("Library.dat", header = TRUE)
+
+# Extract variable P
+P <- data$P
+
+# Sample size
+n <- length(P)
+
+# Compute a and b
+a <- floor(n/2 - 1.96 * sqrt(n/4))
+b <- ceiling(n/2 + 1.96 * sqrt(n/4))
+
+a; b
+
+# Sort data
+P_sorted <- sort(P)
+
+# Extract order statistics
+lower <- P_sorted[a]
+upper <- P_sorted[b]
+
+lower
+upper
+
+# Confidence interval
+c(lower, upper)
